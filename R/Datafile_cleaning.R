c2fe5f79a2b1783da190cf5b1a654b64af67d338
Corinna Grobe <corinna.grobe@statistik.ji.zh.ch>
2021-02-03 01:09:24 +0100

    Fixing handling of NA

--- R/Datafile_cleaning.R
+++ R/Datafile_cleaning.R
@@ -7,15 +7,16 @@ View(data)
 
 cleaning_data <- data %>% 
   mutate(Code_P = paste0(`...1`, `...2`, `...3`),
-         Code_O = case_when(Code_O != "NA" ~ paste0(Code_O, `...11`, `...12`)),
-         Feld_O = `...13`,
+         Code_O = case_when(Code_O != "NA" ~ paste0(Code_O, `...11`, `...12`), TRUE ~ ""),
+         Feld_O = case_when(`...13` != "NA" ~ `...13`, TRUE ~ ""),
          Name_P = trimws(substring(Name_P, 6)),
          Name_O = trimws(substring(Name_O, 6)))
 
 cleaned_data <- cleaning_data[ , -c(1,2,3,11,12,13)]
 View(cleaned_data)
 
-write.table(cleaned_data, "./Data/VSA_Kantonskarte_20210108.csv", 
+write.table(cleaned_data, "./Data/VSA_Kantonskarte_20210108.csv",
+            na = "",
             row.names = FALSE, 
             sep = ",", 
             fileEncoding = "UTF-8")
